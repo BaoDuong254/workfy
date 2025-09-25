c5d1bed22b1335f4bdfa393170e931b83e44a573
GiaBao <duonggiabao254@gmail.com>
2025-09-25 17:11:24 +0700

    Add MongoDB and configuration support; update dependencies and environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>("PORT");
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
   app.setViewEngine("ejs");
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port ?? 3000);
 }
 bootstrap().catch((err) => {
   console.error("Error starting the application:", err);
