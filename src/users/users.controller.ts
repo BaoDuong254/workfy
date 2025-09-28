e5ba2c133115b14f29c2befaec65e0569d5a6c4c
GiaBao <duonggiabao254@gmail.com>
2025-09-28 12:28:36 +0700

    Implement LocalAuthGuard for authentication and update AppController for login handling; remove unused RolesGuard from UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
-import { RolesGuard } from "src/users/test.guard";
 
 @Controller("users")
 export class UsersController {
@@ -13,7 +12,6 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @UseGuards(RolesGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
