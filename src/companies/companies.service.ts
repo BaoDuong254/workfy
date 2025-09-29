ca4e6f0add0ef44154d8eca5919a902ee99bc5a5
GiaBao <duonggiabao254@gmail.com>
2025-09-29 12:03:05 +0700

    feat: integrate Mongoose for Companies module with CRUD operations

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { CreateCompanyDto } from "./dto/create-company.dto";
 import { UpdateCompanyDto } from "./dto/update-company.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Company, CompanyDocument } from "src/companies/schemas/company.schema";
+import type { SoftDeleteModel } from "mongoose-delete";
 
 @Injectable()
 export class CompaniesService {
+  constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) {}
+
   create(createCompanyDto: CreateCompanyDto) {
-    return "This action adds a new company";
+    return this.companyModel.create({ ...createCompanyDto });
   }
 
   findAll() {
