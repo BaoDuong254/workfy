f9747916d7e9ffcd74caacb9cbde9a0e370f4f4b
GiaBao <duonggiabao254@gmail.com>
2025-09-30 16:48:25 +0700

    feat: create User decorator for passing req.user

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,13 +4,20 @@ import { UpdateCompanyDto } from "./dto/update-company.dto";
 import { InjectModel } from "@nestjs/mongoose";
 import { Company, CompanyDocument } from "src/companies/schemas/company.schema";
 import type { SoftDeleteModel } from "mongoose-delete";
+import { IUser } from "src/users/users.interface";
 
 @Injectable()
 export class CompaniesService {
   constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) {}
 
-  create(createCompanyDto: CreateCompanyDto) {
-    return this.companyModel.create({ ...createCompanyDto });
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
   }
 
   findAll() {
