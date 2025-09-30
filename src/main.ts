15dc0d9d7e3b4ff7bc2fa0e32c08aac01a92cc38
GiaBao <duonggiabao254@gmail.com>
2025-09-30 23:46:26 +0700

    feat: enable API versioning in the application

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
 import { ConfigService } from "@nestjs/config";
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { TransformInterceptor } from "src/core/transform.interceptor";
 
@@ -32,6 +32,13 @@ async function bootstrap() {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
+  });
+
+  // Enable API versioning
+  app.setGlobalPrefix("api");
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ["1", "2"],
   });
 
   // Start the application
